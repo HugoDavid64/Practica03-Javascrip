24bee6f46b74ca1fff20ee468849d9851410dca2
Hugo Zhindon <35283790+HugoDavid64@users.noreply.github.com>
2019-11-14 20:16:52 -0500

    Final

--- Imagenes/js/js.js
+++ Imagenes/js/js.js
@@ -26,21 +26,59 @@ function plusDivs(n) {
 function showDivs(n) {
 	var i;
 	var x = document.getElementsByClassName("mySlides");
-	if (n == x.length)
+	if (n == x.length){
 		document.getElementById('right').disabled = true;
-	else
+		desactivarSiguiente();
+	}if(n==1){
 		document.getElementById('right').disabled = false;
-
-	if (n == 1) 
+		ActivarSiguiente();
+	
+	}if (n == 1) {
 		document.getElementById('left').disabled = true;
-	else
+		desactivarAnterior();
+	}
+	if (n==5){
 		document.getElementById('left').disabled = false
-
+		ActivarAnterior();
+	}
+		
+		
 	for (i = 0; i < x.length; i++)
 		x[i].style.display = "none";
 
 	x[slideIndex-1].style.display = "block"; 
 }
+
+function ActivarSiguiente() {
+	document.getElementById("right").disabled = false;
+	document.getElementById("right").style.color = "rgb(255, 255, 255)";
+	document.getElementById("right").style.background = "#1883ba";
+	document.getElementById("right").style.border = "2px solid #999";
+  }
+  
+
+function desactivarSiguiente() {
+	document.getElementById("right").disabled = true;
+	document.getElementById("right").style.color = "rgb(83, 81, 81)";
+	document.getElementById("right").style.background =
+	  " rgb(170, 165, 165)";
+	document.getElementById("right").style.border = "2px solid #ffffff";
+  }
+  
+
+  function ActivarAnterior() {
+	document.getElementById("left").disabled = false;
+	document.getElementById("left").style.color = "rgb(255, 255, 255)";
+	document.getElementById("left").style.background = "#1883ba";
+	document.getElementById("left").style.border = "2px solid #999";
+  }
+  function desactivarAnterior() {
+	document.getElementById("left").disabled = true;
+	document.getElementById("left").style.color = "rgb(83, 81, 81)";
+	document.getElementById("left").style.background =
+	  " rgb(170, 165, 165)";
+	document.getElementById("left").style.border = "2px solid #ffffff";
+  }
 
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
